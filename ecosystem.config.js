472cf907350a39d90a53a661d4518bee7be65243
Mr-luoxi <940280747@qq.com>
2019-03-22 01:19:17 +0800

    change eco

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  apps : [{
+    name: 'ice',
+    script: 'server.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      "user": "root",
+      "host": ["39.97.165.61"],
+      "port": "22",
+      "ref": "origin/master",
+      "repo": "git@github.com:Mr-luoxi/ice.git",
+      "path": "/www/ice/production",
+      "ssh_options": "StrictHostKeyChecking=no",
+      "post-deploy":"git pull",
+      "pre-deploy-local": "echo 'Depoly Done!'",
+      "env": {
+        "NODE_ENV": "production"
+    }
+  }
+}
+}
